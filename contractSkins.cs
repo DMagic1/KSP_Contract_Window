012f5d9a0e3f2654c73525477db97c7d9b9bfae4
DMagic <david.grandy@gmail.com>
2014-08-05 17:11:47 -0400

    Maybe better skin setup

--- contractSkins.cs
+++ contractSkins.cs
@@ -10,24 +10,68 @@ namespace Contracts_Window
 	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
 	class contractSkins: MonoBehaviourExtended
 	{
+		internal static GUIStyle contractActive;
+		internal static GUIStyle contractCompleted;
+		internal static GUIStyle contractFailed;
+
+		internal static GUIStyle paramText;
+		internal static GUIStyle paramCompleted;
+		internal static GUIStyle paramFailed;
+
 		internal override void OnGUIOnceOnly()
 		{
 			GUISkin contractKSP = SkinsLibrary.CopySkin(SkinsLibrary.DefSkinType.KSP);
 			contractKSP.button = SkinsLibrary.DefKSPSkin.button;
 			SkinsLibrary.AddSkin("DefaultSkin", contractKSP);
 
-
-
 			GUISkin contractUnity = SkinsLibrary.CopySkin(SkinsLibrary.DefSkinType.Unity);
 			contractUnity.button = SkinsLibrary.DefUnitySkin.button;
+			contractUnity.box = SkinsLibrary.DefUnitySkin.box;
 			SkinsLibrary.AddSkin("UnitySkin", contractUnity);
 
-			GUIStyle styleText = new GUIStyle(SkinsLibrary.DefUnitySkin.box);
-			styleText.fontSize = 10;
-			styleText.normal.textColor = new Color(200, 200, 200);
-			styleText.wordWrap = true;
+			//Contract Title Skin
+			contractActive = new GUIStyle(SkinsLibrary.DefUnitySkin.button);
+			contractActive.name = "ContractActiveText";
+			contractActive.fontSize = 12;
+			contractActive.alignment = TextAnchor.UpperLeft;
+			contractActive.normal.textColor = XKCDColors.DustyOrange;
+			contractActive.wordWrap = true;
 
-			SkinsLibrary.List["DefaultSkin"].box = new GUIStyle(styleText);
+			contractCompleted = new GUIStyle(contractActive);
+			contractCompleted.name = "ContractCompleteText";
+			contractCompleted.normal.textColor = XKCDColors.FreshGreen;
+
+			contractFailed = new GUIStyle(contractActive);
+			contractFailed.name = "ContractFailedText";
+			contractFailed.normal.textColor = XKCDColors.Red;
+
+			//Parameter Skin
+
+			paramText = new GUIStyle(SkinsLibrary.DefUnitySkin.box);
+			paramText.name = "ParameterText";
+			paramText.fontSize = 11;
+			paramText.normal.textColor = new Color(220, 220, 220);
+			paramText.wordWrap = true;
+			paramText.alignment = TextAnchor.UpperLeft;
+
+			paramCompleted = new GUIStyle(paramText);
+			paramCompleted.name = "ParameterCompleteText";
+			paramCompleted.normal.textColor = XKCDColors.FreshGreen;
+
+			paramFailed = new GUIStyle(paramText);
+			paramFailed.name = "ParameterFailedText";
+			paramFailed.normal.textColor = XKCDColors.Red;
+
+
+			SkinsLibrary.List["UnitySkin"].button = new GUIStyle(contractActive);
+			SkinsLibrary.List["UnitySkin"].box = new GUIStyle(paramText);
+			SkinsLibrary.AddStyle("UnitySkin", "ParameterText", paramText);
+			SkinsLibrary.AddStyle("UnitySkin", "ParameterCompleteText", paramCompleted);
+			SkinsLibrary.AddStyle("UnitySkin", "ParameterFailedText", paramFailed);
+			SkinsLibrary.AddStyle("UnitySkin", "ContractActiveText", contractActive);
+			SkinsLibrary.AddStyle("UnitySkin", "ContractCompleteText", contractCompleted);
+			SkinsLibrary.AddStyle("UnitySkin", "ContractFailedText", contractFailed);
+
 		}
 	}
 }
