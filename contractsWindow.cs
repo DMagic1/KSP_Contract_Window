2c3259a583f9787e8725594abbef9b612a2bbbeb
DMagic <david.grandy@gmail.com>
2014-08-07 13:35:39 -0400

    license, persistence, better notes

--- contractsWindow.cs
+++ contractsWindow.cs
@@ -1,6 +1,33 @@
-﻿using System;
+﻿#region license
+/*The MIT License (MIT)
+Contract Window - Addon to control window for contracts
+
+Copyright (c) 2014 DMagic
+
+KSP Plugin Framework by TriggerAu, 2014: http://forum.kerbalspaceprogram.com/threads/66503-KSP-Plugin-Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -14,11 +41,11 @@ namespace ContractsWindow
 	{
 		internal static bool IsVisible;
 		private List<contractContainer> cList = new List<contractContainer>();
-		private string version, assemblyLocation;
+		private string version, assemblyLocation, assemblyFolder, fileLocation;
 		private Assembly assembly;
 		private Vector2 scroll;
-		private bool resizing;
-		private float dragStart, windowHeight;
+		private bool resizing, visible, fileFound;
+		private float dragStart, windowHeight, windowX, windowY, windowW, windowH, windowMaxY;
 		private Texture2D iconTex;
 		
 		internal override void Awake()
@@ -28,6 +55,11 @@ namespace ContractsWindow
 				assembly = AssemblyLoader.loadedAssemblies.GetByAssembly(Assembly.GetExecutingAssembly()).assembly;
 				assemblyLocation = assembly.Location.Replace("\\", "/");
 				version = FileVersionInfo.GetVersionInfo(assemblyLocation).ProductVersion;
+				assemblyFolder = System.IO.Path.GetDirectoryName(assemblyLocation).Replace("\\", "/");
+				fileLocation = assemblyFolder + "/WindowSettings.cfg";
+				if (System.IO.File.Exists(fileLocation))
+					fileFound = true;
+
 				iconTex = GameDatabase.Instance.GetTexture("Contracts Window/ResizeIcon", false);
 
 				WindowCaption = string.Format("Contracts {0}", version);
@@ -37,6 +69,10 @@ namespace ContractsWindow
 				DragEnabled = true;
 				DragRect = new Rect(WindowRect.x - 35, WindowRect.y - 77, 230, 30);
 				SkinsLibrary.SetCurrent("UnitySkin");
+
+				PersistenceLoad();
+				
+				PersistenceSave();
 			}
 		}
 
@@ -50,11 +86,11 @@ namespace ContractsWindow
 		{
 			GameEvents.Contract.onAccepted.Remove(contractAccepted);
 			GameEvents.Contract.onContractsLoaded.Remove(contractLoaded);
+			PersistenceSave();
 		}
 
 		internal override void Update()
 		{
-			
 		}
 
 		internal override void DrawWindow(int id)
@@ -69,27 +105,30 @@ namespace ContractsWindow
 				if (c.showParams)
 				{
 					GUILayout.BeginVertical();
-					foreach (ContractParameter cP in c.contract.AllParameters)
+					foreach (parameterContainer cP in c.paramList)
 					{
-						if (!string.IsNullOrEmpty(cP.Title))
+						if (!string.IsNullOrEmpty(cP.cParam.Title))
 						{
-							GUILayout.BeginHorizontal();
-							GUILayout.Space(10);
-							GUILayout.Box(cP.Title, paramState(cP.State));
-							GUILayout.EndHorizontal();
-							if (cP.State != ParameterState.Complete && !string.IsNullOrEmpty(cP.Notes))
+							if (cP.cParam.State != ParameterState.Complete && !string.IsNullOrEmpty(cP.cParam.Notes))
 							{
 								GUILayout.BeginHorizontal();
-								if (GUILayout.Button("[+] Note:", contractSkins.noteText, GUILayout.MaxWidth(45)))
-								{
-									c.showNotes = !c.showNotes;
-								}
-								if (c.showNotes)
+								if (GUILayout.Button("[+]", contractSkins.noteButton))
+									cP.showNote = !cP.showNote;
+								GUILayout.Space(5);
+								GUILayout.Box(cP.cParam.Title, paramState(cP.cParam.State));
+								GUILayout.EndHorizontal();
+								if (cP.showNote)
 								{
 									GUILayout.Space(3);
-									GUILayout.Box(cP.Notes, contractSkins.noteText);
+									GUILayout.Box(cP.cParam.Notes, contractSkins.noteText);
 								}
-								GUILayout.EndHorizontal();
+							}
+							else
+							{
+							GUILayout.BeginHorizontal();
+							GUILayout.Space(15);
+							GUILayout.Box(cP.cParam.Title, paramState(cP.cParam.State));
+							GUILayout.EndHorizontal();
 							}
 						}
 					}
@@ -127,6 +166,7 @@ namespace ContractsWindow
 					{
 						resizing = false;
 						WindowRect.yMax = windowHeight;
+						PersistenceSave();
 					}
 					else
 					{
@@ -188,6 +228,70 @@ namespace ContractsWindow
 				}
 			}
 		}
+
+		private void PersistenceLoad()
+		{
+			if (fileFound)
+			{
+				ConfigNode node = ConfigNode.Load(fileLocation);
+				if (node != null)
+				{
+					ConfigNode windowNode = node.GetNode("CONTRACT_WINDOW_SETTINGS");
+					if (windowNode != null)
+					{
+						if (float.TryParse(windowNode.GetValue("Window X"), out windowX))
+							WindowRect.x = windowX;
+						else
+							WindowRect.x = 50;
+						if (float.TryParse(windowNode.GetValue("Window Y"), out windowY))
+							WindowRect.y = windowY;
+						else
+							WindowRect.y = 80;
+						if (float.TryParse(windowNode.GetValue("Window Width"), out windowW))
+							WindowRect.width = windowW;
+						else
+							WindowRect.width = 250;
+						if (float.TryParse(windowNode.GetValue("Window Height"), out windowH))
+							WindowRect.height = windowH;
+						else
+							WindowRect.height = 300;
+						if (float.TryParse(windowNode.GetValue("Window MaxY"), out windowMaxY))
+							WindowRect.yMax = windowMaxY;
+						else
+							WindowRect.yMax = 300;
+						if (bool.TryParse(windowNode.GetValue("Window Visible"), out visible))
+							IsVisible = visible;
+						else
+							IsVisible = false;
+					}
+				}
+			}
+		}
+
+		private void PersistenceSave()
+		{
+			if (fileFound)
+			{
+				ConfigNode node = ConfigNode.Load(fileLocation);
+				if (node != null)
+				{
+					node.ClearNodes();
+					ConfigNode windowNode = new ConfigNode("CONTRACT_WINDOW_SETTINGS");
+					if (windowNode != null)
+					{
+						windowNode.AddValue("Window X", WindowRect.x.ToString());
+						windowNode.AddValue("Window Y", WindowRect.y.ToString());
+						windowNode.AddValue("Window Width", WindowRect.width.ToString());
+						windowNode.AddValue("Window Height", WindowRect.height.ToString());
+						windowNode.AddValue("Window MaxY", WindowRect.yMax.ToString());
+						windowNode.AddValue("Window Visible", IsVisible.ToString());
+						node.AddNode(windowNode);
+						node.Save(fileLocation);
+					}
+				}
+			}
+		}
+
 
 	}
 }
