f8e2f1ac4f70f63bef013be6f6c0e8fe75f07b79
DMagic <david.grandy@gmail.com>
2014-08-07 13:35:22 -0400

    license, note styles

--- contractSkins.cs
+++ contractSkins.cs
@@ -1,6 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿#region license
+/*The MIT License (MIT)
+Contract Skins - Addon to initialize GUISkins and GUIStyles
+
+Copyright (c) 2014 DMagic
+
+KSP Plugin Framework by TriggerAu, 2014: http://forum.kerbalspaceprogram.com/threads/66503-KSP-Plugin-Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
 
 using KSP;
 using UnityEngine;
@@ -19,6 +43,7 @@ namespace ContractsWindow
 		internal static GUIStyle paramFailed;
 
 		internal static GUIStyle noteText;
+		internal static GUIStyle noteButton;
 
 		internal override void OnGUIOnceOnly()
 		{
@@ -66,12 +91,18 @@ namespace ContractsWindow
 
 			//Note Skin
 
-			noteText = new GUIStyle(SkinsLibrary.DefUnitySkin.button);
+			noteText = new GUIStyle(SkinsLibrary.DefUnitySkin.box);
 			noteText.name = "ContractNoteText";
 			noteText.fontSize = 11;
 			noteText.wordWrap = true;
 			noteText.alignment = TextAnchor.UpperLeft;
 			noteText.normal.textColor = XKCDColors.AquaBlue;
+
+			noteButton = new GUIStyle(SkinsLibrary.DefUnitySkin.button);
+			noteButton.name = "ContractNoteButton";
+			noteButton.fontSize = 12;
+			noteButton.alignment = TextAnchor.MiddleCenter;
+			noteButton.normal.textColor = XKCDColors.AquaBlue;
 
 			SkinsLibrary.List["UnitySkin"].button = new GUIStyle(contractActive);
 			SkinsLibrary.List["UnitySkin"].box = new GUIStyle(paramText);
@@ -82,6 +113,7 @@ namespace ContractsWindow
 			SkinsLibrary.AddStyle("UnitySkin", "ContractCompleteText", contractCompleted);
 			SkinsLibrary.AddStyle("UnitySkin", "ContractFailedText", contractFailed);
 			SkinsLibrary.AddStyle("UnitySkin", "ContractNoteText", noteText);
+			SkinsLibrary.AddStyle("UnitySkin", "ContractNoteButton", noteButton);
 
 		}
 	}
