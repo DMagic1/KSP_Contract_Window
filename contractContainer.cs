d2c956f5f12008fb19df9d1ea49f2b0d0b50b4f1
DMagic <david.grandy@gmail.com>
2014-08-08 22:32:57 -0400

    Fix expiration sort, remove unused values

--- contractContainer.cs
+++ contractContainer.cs
@@ -36,8 +36,8 @@ namespace ContractsWindow
 	internal class contractContainer
 	{
 		internal Contract contract;
-		internal float science, repReward, repPenalty, totalScience, totalRepReward, totalRepPenalty;
-		internal double expiration, acceptance, duration, reward, advance, penalty, totalReward, totalPenalty;
+		internal float totalScience, totalRepReward, totalRepPenalty;
+		internal double deadline, totalReward, totalPenalty;
 		internal bool showParams;
 		internal string daysToExpire;
 		internal List<parameterContainer> paramList = new List<parameterContainer>();
@@ -46,16 +46,20 @@ namespace ContractsWindow
 		internal contractContainer(Contract Contract)
 		{
 			contract = Contract;
-			expiration = contract.DateDeadline;
-			daysToExpire = timeInDays(expiration - Planetarium.GetUniversalTime());
-			acceptance = contract.DateAccepted;
-			duration = expiration - acceptance;
-			advance = contract.FundsAdvance;
-			reward = totalReward = contract.FundsCompletion;
-			penalty = totalPenalty = contract.FundsFailure;
-			science = totalScience = contract.ScienceCompletion;
-			repReward = totalRepReward = contract.ReputationCompletion;
-			repPenalty = totalRepPenalty = contract.ReputationFailure;
+
+			//No-expiration date contracts have negative DateExpire values
+			deadline = contract.DateDeadline;
+			if (deadline <= 0 && contract.ContractState == Contracts.Contract.State.Active)
+				deadline = double.MaxValue;
+
+			//Calculate time in day values using Kerbin or Earth days
+			daysToExpire = timeInDays(contract.DateDeadline - Planetarium.GetUniversalTime());
+
+			totalReward = contract.FundsCompletion;
+			totalPenalty = contract.FundsFailure;
+			totalScience = contract.ScienceCompletion;
+			totalRepReward = contract.ReputationCompletion;
+			totalRepPenalty = contract.ReputationFailure;
 			showParams = true;
 
 			foreach (ContractParameter param in contract.AllParameters)
@@ -71,7 +75,7 @@ namespace ContractsWindow
 
 		private string timeInDays(double D)
 		{
-			if (D < 0)
+			if (D <= 0)
 				return "---";
 
 			int[] time = KSPUtil.GetDateFromUT((int)D);
@@ -116,7 +120,7 @@ namespace ContractsWindow
 		Default = 1,
 		Expiration = 2,
 		Acceptance = 3,
-		Duration = 4,
+		Difficulty = 4,
 		Reward = 5,
 	}
 
