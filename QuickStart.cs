2a6e3f0fc462c371128ea79d99ce70ea46aef326
DMagic <david.grandy@gmail.com>
2014-08-15 15:15:29 -0400

    Comment everything out

--- QuickStart.cs
+++ QuickStart.cs
@@ -1,55 +1,55 @@
-﻿#if DEBUG
+﻿//#if DEBUG
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
 
-namespace QuickStart
-{
-    //This will kick us into the save called default and set the first vessel active
+//namespace QuickStart
+//{
+//    //This will kick us into the save called default and set the first vessel active
     
     
-    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+//    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
 
-    public class Debug_AutoLoadPersistentSaveOnStartup : MonoBehaviour {
+//    public class Debug_AutoLoadPersistentSaveOnStartup : MonoBehaviour {
 
-	public static bool first = true;
-	public static int  vId = 0;
+//    public static bool first = true;
+//    public static int  vId = 0;
 
-	public void Start () {
+//    public void Start () {
             
-		if (first) {
-			first = false;
-			HighLogic.SaveFolder = "Contracts Ahoy";
-			var game = GamePersistence.LoadGame ("persistent" , HighLogic.SaveFolder , true , false);
-			if (game != null && game.flightState != null && game.compatible) {
+//        if (first) {
+//            first = false;
+//            HighLogic.SaveFolder = "Contracts Ahoy";
+//            var game = GamePersistence.LoadGame ("persistent" , HighLogic.SaveFolder , true , false);
+//            if (game != null && game.flightState != null && game.compatible) {
                      
-				List<ProtoVessel> allVessels = game.flightState.protoVessels;
-				int suitableVessel = 0;
+//                List<ProtoVessel> allVessels = game.flightState.protoVessels;
+//                int suitableVessel = 0;
 
-				for (vId = 0; vId < allVessels.Count; vId++) {
-					switch (allVessels [vId].vesselType) {
-						case VesselType.SpaceObject: 	continue;  // asteroids
-						case VesselType.Unknown: 	continue;  // asteroids in facepaint
-						case VesselType.EVA: continue;
-						default:					suitableVessel = vId;
-												break; // this one will do
-                         }
-					/* If you want a more stringent filter than
-                      *   "vessel is not inert ball of space dirt", then you
-                      *   will want to do it here.
-                      */
-                    }
-				HighLogic.LoadScene(GameScenes.SPACECENTER);
-				//FlightDriver.StartAndFocusVessel (game , suitableVessel);
-				CheatOptions.InfiniteFuel = true;
-               }
+//                for (vId = 0; vId < allVessels.Count; vId++) {
+//                    switch (allVessels [vId].vesselType) {
+//                        case VesselType.SpaceObject: 	continue;  // asteroids
+//                        case VesselType.Unknown: 	continue;  // asteroids in facepaint
+//                        case VesselType.EVA: continue;
+//                        default:					suitableVessel = vId;
+//                                                break; // this one will do
+//                         }
+//                    /* If you want a more stringent filter than
+//                      *   "vessel is not inert ball of space dirt", then you
+//                      *   will want to do it here.
+//                      */
+//                    }
+//                HighLogic.LoadScene(GameScenes.SPACECENTER);
+//                //FlightDriver.StartAndFocusVessel (game , suitableVessel);
+//                CheatOptions.InfiniteFuel = true;
+//               }
 
-          }
-     }
-}
-}
+//          }
+//     }
+//}
+//}
   
 
-#endif
+//#endif
