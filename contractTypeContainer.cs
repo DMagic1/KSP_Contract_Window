929ba8fd81f91bc3cc3aaf965b92567202ad036a
DMagic <david.grandy@gmail.com>
2014-12-11 17:32:41 -0500

    containers to store contract and param info

---
+++ contractTypeContainer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Contracts;
+using Contracts.Parameters;
+using Contracts.Templates;
+using UnityEngine;
+
+namespace ContractsWindow
+{
+	class contractTypeContainer
+	{
+		private Type contractType;
+		private string name;
+		private Contract contractC;
+		private float rewardFund, penaltyFund, advanceFund, rewardRep, penaltyRep, rewardScience;
+		private float durationTime, expirationTime, maxOffer, maxActive;
+
+		internal contractTypeContainer (Type CType)
+		{
+			contractType = CType;
+			contractC = (Contract)Activator.CreateInstance(CType);
+			name = CType.Name;
+		}
+
+		public Contract ContractC
+		{
+			get { return contractC; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+		}
+
+		public float RewardFund
+		{
+			get { return rewardFund; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardFund = value;
+			}
+		}
+
+		public float PenaltyFund
+		{
+			get { return penaltyFund; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					penaltyFund = value;
+			}
+		}
+
+		public float AdvanceFund
+		{
+			get { return advanceFund; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					advanceFund = value;
+			}
+		}
+
+		public float RewardRep
+		{
+			get { return rewardRep; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardRep = value;
+			}
+		}
+
+		public float PenaltyRep
+		{
+			get { return penaltyRep; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					penaltyRep = value;
+			}
+		}
+
+		public float RewardScience
+		{
+			get { return rewardScience; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardScience = value;
+			}
+		}
+
+		public float DurationTime
+		{
+			get { return durationTime; }
+			set
+			{
+				if (value > 0 && value < 1000)
+					durationTime = value;
+			}
+		}
+
+		public float ExpirationTime
+		{
+			get { return expirationTime; }
+			set
+			{
+				if (value > 0 && value < 1000)
+					expirationTime = value;
+			}
+		}
+
+		public float MaxOffer
+		{
+			get { return maxOffer; }
+			set
+			{
+				if (value >= 0 && value < 50)
+					maxOffer = value;
+			}
+		}
+
+		public float MaxActive
+		{
+			get { return maxActive; }
+			set
+			{
+				if (value >= 0 && value < 50)
+					maxActive = value;
+			}
+		}
+
+
+	}
+}
---
+++ paramTypeContainer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Contracts;
+using Contracts.Parameters;
+using UnityEngine;
+
+namespace ContractsWindow
+{
+	class paramTypeContainer
+	{
+		private Type paramType;
+		private ContractParameter param;
+		private string name;
+		private float rewardFund, penaltyFund, rewardRep, penaltyRep, rewardScience;
+
+		internal paramTypeContainer (Type PType)
+		{
+			paramType = PType;
+			param = (ContractParameter)Activator.CreateInstance(PType);
+			name = PType.Name;
+		}
+
+		public ContractParameter Param
+		{
+			get { return param; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+		}
+
+		public float RewardFund
+		{
+			get { return rewardFund; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardFund = value;
+			}
+		}
+
+		public float PenaltyFund
+		{
+			get { return penaltyFund; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					penaltyFund = value;
+			}
+		}
+
+		public float RewardRep
+		{
+			get { return rewardRep; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardRep = value;
+			}
+		}
+
+		public float PenaltyRep
+		{
+			get { return penaltyRep; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					penaltyRep = value;
+			}
+		}
+
+		public float RewardScience
+		{
+			get { return rewardScience; }
+			set
+			{
+				if (value >= 0 && value < 1000)
+					rewardScience = value;
+			}
+		}
+	}
+}
