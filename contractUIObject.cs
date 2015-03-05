13bd9378db8e94df28aa303dd9b323af2e51f21c
DMagic <david.grandy@gmail.com>
2015-03-05 16:12:05 -0500

    New object to store UI state

---
+++ contractUIObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContractsWindow
+{
+	public class contractUIObject
+	{
+		private contractContainer container;
+		private bool showParams;
+		private int? order;
+
+		internal contractUIObject(contractContainer c)
+		{
+			container = c;
+		}
+
+		public contractContainer Container
+		{
+			get { return container; }
+		}
+
+		public bool ShowParams
+		{
+			get { return showParams; }
+			internal set { showParams = value; }
+		}
+
+		public int? Order
+		{
+			get { return order; }
+			internal set { order = value; }
+		}
+	}
+}
