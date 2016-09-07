75a7cec71593cbd11e1b24083996a10839ae22cb
DMagic <david.grandy@gmail.com>
2016-09-07 19:58:42 -0400

    Asset bundle loader

---
+++ Source/ContractsWindow/contractLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace ContractsWindow
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, false)]
+	public class contractLoader : MonoBehaviour
+	{
+		private static AssetBundle images;
+		private static AssetBundle prefabs;
+
+		public static AssetBundle Images
+		{
+			get { return images; }
+		}
+
+		public static AssetBundle Prefabs
+		{
+			get { return prefabs; }
+		}
+
+		private void Awake()
+		{
+			string path = KSPUtil.ApplicationRootPath + "GameData/DMagicUtilities/ContractsWindow/Resources";
+
+			images = AssetBundle.CreateFromFile(path + "/cw_images.ksp");
+			prefabs = AssetBundle.CreateFromFile(path + "/cw_prefabs.ksp");
+		}
+	}
+}
