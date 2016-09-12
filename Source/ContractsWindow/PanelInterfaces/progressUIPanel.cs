8b527e31d9cb9544d51ecaa29386af753bc0b70f
DMagic <david.grandy@gmail.com>
2016-09-12 18:30:24 -0400

    Assign different progress node types

--- Source/ContractsWindow/PanelInterfaces/progressUIPanel.cs
+++ Source/ContractsWindow/PanelInterfaces/progressUIPanel.cs
@@ -8,22 +8,143 @@ namespace ContractsWindow.PanelInterfaces
 {
 	public class progressUIPanel : IProgressPanel
 	{
-
-		private List<IBodyNodes> bodies = new List<IBodyNodes>();
-		private List<IIntervalType> intervals = new List<IIntervalType>();
+		private bool _isVisible;
+		private Dictionary<string, List<IStandardNode>> bodies = new Dictionary<string, List<IStandardNode>>();
+		private List<IIntervalNode> intervals = new List<IIntervalNode>();
 		private List<IStandardNode> pois = new List<IStandardNode>();
 		private List<IStandardNode> standards = new List<IStandardNode>();
-
-		public bool IsVisible { get; set; }
-
-		public IList<IBodyNodes> GetBodies
-		{
-			get { return new List<IBodyNodes>(bodies.ToArray()); }
+		
+		public progressUIPanel()
+		{			
+			loadIntervals(progressParser.getAllIntervalNodes);
+			loadStandards(progressParser.getAllStandardNodes);
+			loadPOIs(progressParser.getAllPOINodes);
+			loadBodies(progressParser.getAllBodyNodes);
 		}
 
-		public IList<IIntervalType> GetIntervalTypes
+		private void loadIntervals(List<progressInterval> nodes)
 		{
-			get { return new List<IIntervalType>(intervals.ToArray()); }
+			int l = nodes.Count;
+
+			for (int i = 0; i < l; i++)
+			{
+				IntervalNodeUI node = new IntervalNodeUI(nodes[i]);
+
+				if (node == null)
+					continue;
+
+				intervals.Add(node);
+			}
+		}
+
+		private void loadStandards(List<progressStandard> nodes)
+		{
+			int l = nodes.Count;
+
+			for (int i = 0; i < l; i++)
+			{
+				StandardNodeUI node = new StandardNodeUI(nodes[i]);
+
+				if (node == null)
+					continue;
+
+				standards.Add(node);
+			}
+		}
+
+		private void loadPOIs(List<progressStandard> nodes)
+		{
+			int l = nodes.Count;
+
+			for (int i = 0; i < l; i++)
+			{
+				StandardNodeUI node = new StandardNodeUI(nodes[i]);
+
+				if (node == null)
+					continue;
+
+				pois.Add(node);
+			}
+		}
+
+		private void loadBodies(List<progressBodyCollection> nodes)
+		{
+			int l = nodes.Count;
+
+			for (int i = 0; i < l; i++)
+			{
+				progressBodyCollection body = nodes[i];
+
+				if (body == null)
+					continue;
+
+				if (bodies.ContainsKey(body.Body.bodyName))
+					continue;
+
+				List<progressStandard> bodySubNodes = body.getAllNodes;
+
+				List<IStandardNode> newNodes = new List<IStandardNode>();
+
+				int k = bodySubNodes.Count;
+
+				for (int j = 0; j < k; j++)
+				{
+					StandardNodeUI node = new StandardNodeUI(bodySubNodes[j]);
+
+					if (node == null)
+						continue;
+
+					newNodes.Add(node);
+				}
+
+				bodies.Add(body.Body.bodyName, newNodes);
+			}
+		}
+
+		public bool IsVisible
+		{
+			get { return _isVisible; }
+			set { _isVisible = value; }
+		}
+
+		public bool AnyInterval
+		{
+			get { return progressParser.AnyInterval; }
+		}
+
+		public bool AnyPOI
+		{
+			get { return progressParser.AnyPOI; }
+		}
+
+		public bool AnyStandard
+		{
+			get { return progressParser.AnyStandard; }
+		}
+
+		public bool AnyBody
+		{
+			get { return progressParser.AnyBody; }
+		}
+
+		public bool AnyBodyNode(string s)
+		{
+			progressBodyCollection body = progressParser.getProgressBody(s);
+
+			if (body == null)
+				return false;
+
+			return body.IsReached;
+		}
+		
+		public Dictionary<string, List<IStandardNode>> GetBodies
+		{
+			get { return bodies; }
+		}
+
+		public IList<IIntervalNode> GetIntervalNodes
+		{
+			get { return new List<IIntervalNode>(intervals.ToArray()); }
 		}
 
 		public IList<IStandardNode> GetPOINodes
@@ -35,6 +156,5 @@ namespace ContractsWindow.PanelInterfaces
 		{
 			get { return new List<IStandardNode>(standards.ToArray()); }
 		}
-
 	}
 }
