f72efb6fbc85df3d49150ebf35d351149d3f9f1a
DMagic <david.grandy@gmail.com>
2016-09-21 18:33:07 -0400

    Unused

--- Source/ContractsWindow/contractLoader.cs
+++ Source/ContractsWindow/contractLoader.cs
@@ -7,20 +7,7 @@ namespace ContractsWindow
 	[KSPAddon(KSPAddon.Startup.Instantly, false)]
 	public class contractLoader : MonoBehaviour
 	{
-		private static AssetBundle images;
 		private static AssetBundle prefabs;
-
-		private static Texture2D toolbarIcon;
-
-		public static AssetBundle Images
-		{
-			get { return images; }
-		}
-
-		public static Texture2D ToolbarIcon
-		{
-			get { return toolbarIcon; }
-		}
 
 		public static AssetBundle Prefabs
 		{
@@ -31,8 +18,7 @@ namespace ContractsWindow
 		{
 			string path = KSPUtil.ApplicationRootPath + "GameData/DMagicUtilities/ContractsWindow/Resources";
 
-			images = AssetBundle.CreateFromFile(path + "/cw_images.ksp");
-			prefabs = AssetBundle.CreateFromFile(path + "/contracts_window_prefabs.ksp");
+			prefabs = AssetBundle.LoadFromFile(path + "/contracts_window_prefabs.ksp");
 		}
 	}
 }
