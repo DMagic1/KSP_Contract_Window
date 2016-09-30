7de4e5ffd4c955549d23e5be33facfd1eab14017
DMagic <david.grandy@gmail.com>
2016-09-30 18:42:59 -0400

    License info; mission order

--- Source/ContractsWindow.Unity/Unity/CW_MissionAdd.cs
+++ Source/ContractsWindow.Unity/Unity/CW_MissionAdd.cs
@@ -1,5 +1,32 @@
-﻿using System;
+﻿#region license
+/*The MIT License (MIT)
+CW_MissionAdd - Controls the contract mission contract add popup
+
+Copyright (c) 2016 DMagic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using ContractsWindow.Unity.Interfaces;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,7 +66,9 @@ namespace ContractsWindow.Unity.Unity
 			if (MissionObjectTransform == null)
 				return;
 
-			for (int i = missions.Count - 1; i >= 0; i--)
+			int l = missions.Count;
+
+			for (int i = 0; i < l; i++)
 			{
 				IMissionSection mission = missions[i];
 
@@ -71,10 +100,10 @@ namespace ContractsWindow.Unity.Unity
 		{
 			if (CW_Window.Window == null)
 				return;
-			
-			CW_Window.Window.showCreator(contractInterface);
 
 			DestroyPanel();
+			
+			CW_Window.Window.showCreator(contractInterface);
 		}
 
 		public void DestroyPanel()
