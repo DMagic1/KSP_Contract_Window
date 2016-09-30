cb9360f377d48a26d0ddb614e131871099955c26
DMagic <david.grandy@gmail.com>
2016-09-30 18:41:17 -0400

    License info; object processing

--- Source/ContractsWindow.Unity/Interfaces/IIntervalNode.cs
+++ Source/ContractsWindow.Unity/Interfaces/IIntervalNode.cs
@@ -1,5 +1,34 @@
-﻿using System;
+﻿#region license
+/*The MIT License (MIT)
+Contract Assembly - Monobehaviour To Check For Other Addons And Their Methods
+
+Copyright (c) 2014 DMagic
+
+KSP Plugin Framework by TriggerAu, 2014: http://forum.kerbalspaceprogram.com/threads/66503-KSP-Plugin-Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace ContractsWindow.Unity.Interfaces
 {
@@ -16,5 +45,7 @@ namespace ContractsWindow.Unity.Interfaces
 		string NodeValue(int i);
 
 		string RewardText(int i);
+
+		void ProcessStyles(GameObject obj);
 	}
 }
