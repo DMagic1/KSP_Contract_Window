1430b63ab09a666ea76a8cf00a6c6bfa5545ad7f
DMagic <david.grandy@gmail.com>
2016-09-30 18:43:09 -0400

    New file for updating text info

---
+++ Source/ContractsWindow.Unity/TextHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace ContractsWindow.Unity
+{
+	public class TextHandler : MonoBehaviour
+	{
+		public class OnTextEvent : UnityEvent<string> { }
+
+		public class OnColorEvent : UnityEvent<Color> { }
+
+		public class OnFontEvent : UnityEvent<int> { }
+
+		private OnTextEvent _onTextUpdate = new OnTextEvent();
+		private OnColorEvent _onColorUpdate = new OnColorEvent();
+		private OnFontEvent _onFontChange = new OnFontEvent();
+
+		public UnityEvent<string> OnTextUpdate
+		{
+			get { return _onTextUpdate; }
+		}
+
+		public UnityEvent<Color> OnColorUpdate
+		{
+			get { return _onColorUpdate; }
+		}
+
+		public UnityEvent<int> OnFontChange
+		{
+			get { return _onFontChange; }
+		}
+	}
+}
