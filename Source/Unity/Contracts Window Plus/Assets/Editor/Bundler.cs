855d0ea3b06b0f2b87ad59b677609bd3944e5634
DMagic <david.grandy@gmail.com>
2018-03-16 18:42:55 -0400

    Update Unity project to 2017.1

---
+++ Source/Unity/Contracts Window Plus/Assets/Editor/Bundler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+public class Bundler
+{
+	const string dir = "AssetBundles";
+	const string extension = ".cwp";
+
+    [MenuItem("Contracts Window +/Build Bundles")]
+    static void BuildAllAssetBundles()
+    {
+		BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.StandaloneWindows);
+
+		FileUtil.ReplaceFile(dir + "/contracts_window_prefabs", dir + "/contracts_window_prefabs" + extension);
+        FileUtil.ReplaceFile(dir + "/unity_skin", dir + "/unity_skin" + extension);
+
+        FileUtil.DeleteFileOrDirectory(dir + "/contracts_window_prefabs");
+        FileUtil.DeleteFileOrDirectory(dir + "/unity_skin");
+    }
+}
