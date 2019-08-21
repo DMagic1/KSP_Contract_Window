9ced03898c992fc1872a213329053c5a55a41da2
DMagic1 <david.grandy@gmail.com>
2019-08-21 13:00:59 -0700

    Version

--- Source/ContractsWindow/Properties/AssemblyInfo.cs
+++ Source/ContractsWindow/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f18527dd-a54e-4711-9dcd-c73a221ebb2d")]
 
-[assembly: AssemblyVersion("1.0.9.2")]
-[assembly: AssemblyFileVersion("1.0.9.2")]
-[assembly: AssemblyInformationalVersion("v9.2")]
-[assembly: KSPAssembly("Contracts_Window", 9, 2)]
+[assembly: AssemblyVersion("1.0.9.3")]
+[assembly: AssemblyFileVersion("1.0.9.3")]
+[assembly: AssemblyInformationalVersion("v9.3")]
+[assembly: KSPAssembly("Contracts_Window", 9, 3)]
 [assembly: KSPAssemblyDependency("Progress Parser", 0, 10)]
 [assembly: KSPAssemblyDependency("Contract Parser", 0, 8)]
